594cd6a0c90d6852e195a66cfb8143fc30b02ecd
sternmate2 <shaharstern@hotmail.com>
2021-11-02 12:54:11 +0200

    statefile

--- main.tf
+++ main.tf
@@ -185,35 +185,35 @@ resource "azurerm_virtual_machine" "test" {
 #############################################################################
 
 
-data "azurerm_virtual_network" "shaharbastion" {
-  name                = "bastion1"
-  resource_group_name = "bastion1"
-}
+# data "azurerm_virtual_network" "shaharbastion" {
+#   name                = "bastion1"
+#   resource_group_name = "bastion1"
+# }
 
-data "azurerm_resource_group" "shaharpeering" {
-   name                = "bastion1"
-}
+# data "azurerm_resource_group" "shaharpeering" {
+#    name                = "bastion1"
+# }
 
 
-resource "azurerm_virtual_network_peering" "shaharbastion" {
-  name                      = "peertobastion1"
-  resource_group_name       = azurerm_resource_group.test.name
-  virtual_network_name      = module.test.vnet_name
-  remote_virtual_network_id = data.azurerm_virtual_network.shaharbastion.id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  allow_gateway_transit = false
-}
+# resource "azurerm_virtual_network_peering" "shaharbastion" {
+#   name                      = "peertobastion1"
+#   resource_group_name       = azurerm_resource_group.test.name
+#   virtual_network_name      = module.test.vnet_name
+#   remote_virtual_network_id = data.azurerm_virtual_network.shaharbastion.id
+#   allow_virtual_network_access = true
+#   allow_forwarded_traffic      = true
+#   allow_gateway_transit = false
+# }
 
-resource "azurerm_virtual_network_peering" "shaharpeering" {
-  name                      = "peertoShaharTF"
-  resource_group_name       = data.azurerm_resource_group.shaharpeering.name
-  virtual_network_name      = data.azurerm_virtual_network.shaharbastion.name
-  remote_virtual_network_id = module.test.vnet_id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  allow_gateway_transit = false
-}
+# resource "azurerm_virtual_network_peering" "shaharpeering" {
+#   name                      = "peertoShaharTF"
+#   resource_group_name       = data.azurerm_resource_group.shaharpeering.name
+#   virtual_network_name      = data.azurerm_virtual_network.shaharbastion.name
+#   remote_virtual_network_id = module.test.vnet_id
+#   allow_virtual_network_access = true
+#   allow_forwarded_traffic      = true
+#   allow_gateway_transit = false
+# }
 
 #############################################################################
 # LoadBalncer + BackEndPool + LB rules
